9b9156a15c426ce05d46ff61ece30c2d69b7c01d
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-01-22 14:34:48 +0800

    Finished Code!

--- exercise4.py
+++ exercise4.py
@@ -1,3 +1,8 @@
 # Define a function that takes two parameter
 # first parameter is the word, second parameter is the target index
+def remove_chars(word, index):
+    return word[index:]
 # simply return the sliced word starting from the given index 
+
+remove_chars("pynative", 4) # Output: tive
+remove_chars("pynative", 2) # Output native
